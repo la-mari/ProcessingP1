66f38ec1f2e00abb1aefb432f1580fb2fa08188f
la-mari <karima19@gmail.com>
2016-06-26 18:38:05 -0700

    add transparency

--- sketch.js
+++ sketch.js
@@ -7,10 +7,18 @@ function setup() {
 
 
 function draw(){
-	createCanvas(1024, 768)
-	background(0);
-	ellipse(50, 50, 150, 150)
-	rect(100, 100, 100, 100)
-	strokeWeight(10)
-	line(0,0, 500, 500)
+	createCanvas(1024, 768);
+	background(100);
+	strokeWeight(10);
+
+	fill(255, 255, 0);
+	rect(100, 100, 300, 300);
+
+	fill(255, 0, 0, 100); //fourth fill element is transparency
+	rect(250, 250, 300, 300);
+
+	fill(200, 100); //two arguments is grayscale with transparency
+	rect(150, 150, 300, 300);
+
+
 }
