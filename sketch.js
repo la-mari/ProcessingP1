93da83fb147ec2dd95d013f724609fbf26d2cff5
la-mari <karima19@gmail.com>
2016-06-26 18:29:18 -0700

    start sketch

--- sketch.js
+++ sketch.js
@@ -1,1 +1,16 @@
 console.log("js page linked");
+
+function setup() {
+
+}
+
+
+
+function draw(){
+	createCanvas(1024, 768)
+	background(0);
+	ellipse(50, 50, 150, 150)
+	rect(100, 100, 100, 100)
+	strokeWeight(10)
+	line(0,0, 500, 500)
+}
